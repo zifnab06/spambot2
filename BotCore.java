147672929d69ec20c2f3965670904a7cb68d00aa
Tom Powell <zifnab@zifnab06.net>
2012-05-15 23:36:35 -0600

    meh

--- BotCore.java
+++ BotCore.java
@@ -11,90 +11,92 @@ public class BotCore extends PircBot {
 	private static final int WARNINGS_TO_GIVE = 2;
 
 	private ArrayList<String> userNames = new ArrayList<String>();
-    	private HashMap<String,Integer> userWarnings = new HashMap<String,Integer>();
-   	private boolean userInArray = false;
+		private HashMap<String,Integer> userWarnings = new HashMap<String,Integer>();
+	   private boolean userInArray = false;
+	private String[] BAD_WORDS;
 
-    public BotCore() {
-    	this.setName("Mother");
+	public BotCore() {
+			this.setName("Mother");
+		BAD_WORDS = readPartyList();
+
 	}
 
-    public static String readPartyList() {
-	//start reader
-	File file = new File("swearWords.txt");
-                StringBuffer contents = new StringBuffer();
-                BufferedReader reader = null;
+	public static String[] readPartyList() {
+		//start reader
+		File file = new File("swearWords.txt");
+		StringBuffer contents = new StringBuffer();
+		BufferedReader reader = null;
 
-        try {
-                reader = new BufferedReader(new FileReader(file));
-                String txt = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String txt = null;
 
-                //repeat until all lines are read
-                while ((txt = reader.readLine()) !=null) {
-                        contents.append(txt)
-                                        .append(System.getProperty("line.separator"));
-                }
-        } catch (FileNotFoundException e) {
-                e.printStackTrace();
-        } catch (IOException e) {
-                e.printStackTrace();
-        } finally {
-                try {
-                        if (reader != null) {
-                                reader.close();
-                        }
-
+			//repeat until all lines are read
+			while ((txt = reader.readLine()) !=null) {
+				contents.append(txt)
+					.append("|");
+			  }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-                        e.printStackTrace();
-                }
-        }
-		String BAD_WORDS = contents.toString();
-		return BAD_WORDS;
-    }
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
 
-    public void onMessage(String channel, String sender,
-            String login, String hostname, String message, String BAD_WORDS) {
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		String[] BAD_WORDS_RETURN = contents.toString().split("|");
+		return BAD_WORDS_RETURN;
+		}
 
-	List<String> NAUGHTY = Array.asList(BAD_NAMES);
+	public void onMessage(String channel, String sender,
+		String login, String hostname, String message) {
 
-    	if(Arrays.asList(NAUGHTY).contains(message.toLowerCase())){
-
-
-    		String userInfo;
-    		if(hostname.contains("gateway/web/freenode")){
-                userInfo = login;
-    		}else{
-                userInfo = hostname;
-    		}
-
-    		//Check if user is in the ArrayList
-    		if(userNames.contains(userInfo)){
-    			int warnings = userWarnings.get(userInfo);
-    			warnings++;
-    			userWarnings.put(userInfo, warnings);
-    			if(warnings > WARNINGS_TO_GIVE){
-    				banUser(hostname,channel,sender,userInfo);
-    			}else{
-    				warnUser(channel,sender,warnings);
-    			}
-    		}else{
-    			//If not in ArrayList, add user
-    			userNames.add(userInfo);
-    			//Once user is in ArrayList, add 1 to warning
-    			userWarnings.put(userInfo, 1);
-    			warnUser(channel, sender,1);
-    		}
-    	} else if (sender.equals("zifnab") || sender.equals("jcase") || sender.equals("dougpiston")) {
-		if (message.contains("!clear")) {
-			userWarnings.clear();
-                        userNames.clear();
-
+		for (String bad : BAD_WORDS) {
+			if (message.toLowerCase().contains(bad.toLowerCase())){
+				String userInfo;
+				userInfo = hostname;
+				//Check if user is in the ArrayList
+				if(userNames.contains(userInfo)){
+					int warnings = userWarnings.get(userInfo);
+					warnings++;
+					userWarnings.put(userInfo, warnings);
+					if(warnings > WARNINGS_TO_GIVE){
+						banUser(hostname,channel,sender,userInfo);
+					}else{
+						warnUser(channel,sender,warnings);
+					}
+				}else{
+					//If not in ArrayList, add user
+					userNames.add(userInfo);
+					//Once user is in ArrayList, add 1 to warning
+					userWarnings.put(userInfo, 1);
+					warnUser(channel, sender,1);
+				}
+			}
+		}
+		if (sender.equals("zifnab") || sender.equals("jcase") || sender.equals("dougpiston")) {
+			if (message.contains("!clear")) {
+				userWarnings.clear();
+				userNames.clear();
+			}
+			if (message.contains("!update")){
+				BAD_WORDS = null;
+				BAD_WORDS = readPartyList();
+			}
 		}
 	}
-    }
+
+
 
 	private void banUser(String hostname, String channel, String sender, String userInfo) {
 		setMode(channel, "+q *" + userInfo + "*");
-		kick(channel, sender, "You have sworn too many times in this channel. You have been quieted and cannot speak.");
+		sendMessage(channel, sender + ", you have sworn too many times in this channel. You have been quieted and cannot speak.");
 	}
 
 	private void warnUser(String channel, String sender, int warnings) {
