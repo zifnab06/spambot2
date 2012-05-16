deb0408b4f9917edfd4c7438eb4b3950ef3e2f88
Tom Powell <zifnab@zifnab06.net>
2012-05-15 23:09:05 -0600

    added dougpiston's changes

--- BotCore.java
+++ BotCore.java
@@ -1,57 +1,96 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.jibble.pircbot.*;
 import java.util.*;
 
 public class BotCore extends PircBot {
 
-	private static final String[] BAD_WORDS = new String[]{"fuck","bitch","whore",
-		"asshole","cum","cock","shit","fag","nigger","twat",
-		"cunt","douche","slut","dick","nigga"};
 	private static final int WARNINGS_TO_GIVE = 2;
 
 	private ArrayList<String> userNames = new ArrayList<String>();
-	private HashMap<String,Integer> userWarnings = new HashMap<String,Integer>();
-	private boolean userInArray = false;
+    	private HashMap<String,Integer> userWarnings = new HashMap<String,Integer>();
+   	private boolean userInArray = false;
 
-	public BotCore() {
-		this.setName("SpamGuard");
+    public BotCore() {
+    	this.setName("Mother");
 	}
-	public void onMessage(String channel, String sender,
-		String login, String hostname, String message) {
 
-	for (String word : BAD_WORDS) {
-		if (message.contains(word)){
-			String userInfo;
-			if(hostname.contains("gateway/web/freenode")){
-				userInfo = login;
-			}else{
-				userInfo = hostname;
-			}
-		//Check if user is in the ArrayList
-			if(userNames.contains(userInfo)){
-				int warnings = userWarnings.get(userInfo);
-				warnings++;
-				userWarnings.put(userInfo, warnings);
-				if(warnings > WARNINGS_TO_GIVE){
-					banUser(hostname,channel,sender,userInfo);
-				}else{
-					warnUser(channel,sender,warnings);
-				}
-			}else{
-				//If not in ArrayList, add user
-				userNames.add(userInfo);
-				//Once user is in ArrayList, add 1 to warning
-				userWarnings.put(userInfo, 1);
-				warnUser(channel, sender,1);
-			}
-		} else if (sender.equals("zifnab") || sender.equals("jcase") || sender.equals("dougpiston")) {
-			if (message.contains("!clear")) {
-				userWarnings.clear();
-				userNames.clear();
+    public static String readPartyList() {
+	//start reader
+	File file = new File("swearWords.txt");
+                StringBuffer contents = new StringBuffer();
+                BufferedReader reader = null;
 
-			}
+        try {
+                reader = new BufferedReader(new FileReader(file));
+                String txt = null;
+
+                //repeat until all lines are read
+                while ((txt = reader.readLine()) !=null) {
+                        contents.append(txt)
+                                        .append(System.getProperty("line.separator"));
+                }
+        } catch (FileNotFoundException e) {
+                e.printStackTrace();
+        } catch (IOException e) {
+                e.printStackTrace();
+        } finally {
+                try {
+                        if (reader != null) {
+                                reader.close();
+                        }
+
+		} catch (IOException e) {
+                        e.printStackTrace();
+                }
+        }
+		String BAD_WORDS = contents.toString();
+		return BAD_WORDS;
+    }
+
+    public void onMessage(String channel, String sender,
+            String login, String hostname, String message, String BAD_WORDS) {
+
+	List<String> NAUGHTY = Array.asList(BAD_NAMES);
+
+    	if(Arrays.asList(NAUGHTY).contains(message.toLowerCase())){
+
+
+    		String userInfo;
+    		if(hostname.contains("gateway/web/freenode")){
+                userInfo = login;
+    		}else{
+                userInfo = hostname;
+    		}
+
+    		//Check if user is in the ArrayList
+    		if(userNames.contains(userInfo)){
+    			int warnings = userWarnings.get(userInfo);
+    			warnings++;
+    			userWarnings.put(userInfo, warnings);
+    			if(warnings > WARNINGS_TO_GIVE){
+    				banUser(hostname,channel,sender,userInfo);
+    			}else{
+    				warnUser(channel,sender,warnings);
+    			}
+    		}else{
+    			//If not in ArrayList, add user
+    			userNames.add(userInfo);
+    			//Once user is in ArrayList, add 1 to warning
+    			userWarnings.put(userInfo, 1);
+    			warnUser(channel, sender,1);
+    		}
+    	} else if (sender.equals("zifnab") || sender.equals("jcase") || sender.equals("dougpiston")) {
+		if (message.contains("!clear")) {
+			userWarnings.clear();
+                        userNames.clear();
+
 		}
 	}
-}
+    }
 
 	private void banUser(String hostname, String channel, String sender, String userInfo) {
 		setMode(channel, "+q *" + userInfo + "*");
@@ -63,30 +102,3 @@ public class BotCore extends PircBot {
 	}
 }
 
-
-
-/*import org.jibble.pircbot.*;
-import java.util.Arrays;
-import java.util.*;
-
-public class BotCore extends PircBot {
-	String[] swear = {"anal", "anus", "arse", "ass", "ballsack", "bastard", "bitch", "biatch", "blowjob", "blow job", "bollock", "bollok", "boner", "boob", "bugger", "bum", "butt", "buttplug", "clitoris", "clit", "cock", "coon", "cunt", "dick", "dildo", "dyke", "fag", "fellate", "fellatio", "felching", "fuck", "f u c k", "fudgepacker", "fudge packer", "flange", "Goddamn", "God damn", "homo", "labia", "muff", "nigger", "nigga", "penis", "prick", "pussy", "queer", "scrotum", "shit", "s hit", "sh1t", "slut", "smegma", "spunk", "tosser", "twat", "vagina", "wank", "whore"};
-	ArrayList<String> AL = new ArrayList<String>();
-public BotCore() {
-this.setName("SpamGuard");
-}
-	public void onMessage(String channel, String sender, String login, String hostname, String message) {
-		if (channel.equals("#rootz")){
-			for (int i=0; i<swear.length; i++) {
-				if (message.contains(swear[i]);
-					//start thread
-					//quiet
-					//sleep 30
-					//unquiet
-					}
-				}
-			}
-		}
-	}
-}
-*/
